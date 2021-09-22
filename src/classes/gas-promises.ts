7362743d66879b47044b67ae4571dcd2bc20820a
Evan O'Brien <evanbobrien@gmail.com>
2021-09-22 02:30:45 +0100

    Add option to reroute serverFunctions

--- src/classes/gas-promises.ts
+++ src/classes/gas-promises.ts
@@ -2,21 +2,24 @@ import { ignoredFunctionNames } from '../utils/ignored-function-names';
 import { promisify } from '../utils/promisify';
 import { FunctionHost } from './function-host';
 import { FunctionMap, ServerFunctions } from '../types/functions';
+import { ServerConfig } from '../types/config';
 
 class GASPromises<FM extends FunctionMap> extends FunctionHost<FM> {
-  constructor() {
+  constructor(private config?: ServerConfig) {
     super();
-    this.promisifyGASFunctions();
+    this.promisifyGASFunctions(config);
   }
 
-  private promisifyGASFunctions(): void {
+  private promisifyGASFunctions(config: ServerConfig = {}): void {
+    const route = config.reroute ?? ((func: string) => func);
+
     this._serverFunctions = Object.keys(google.script.run).reduce(
       (acc, functionName) =>
         ignoredFunctionNames.includes(functionName)
           ? acc
           : {
               ...acc,
-              [functionName]: promisify(functionName),
+              [functionName]: promisify(route(functionName)),
             },
       {}
     ) as ServerFunctions<FM>;
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,7 +1,8 @@
 type AllowedDevelopmentDomains = string | ((origin: string) => boolean);
 
 interface ServerConfig {
-  allowedDevelopmentDomains: AllowedDevelopmentDomains;
+  allowedDevelopmentDomains?: AllowedDevelopmentDomains;
+  reroute?: (functionName: string) => string;
 }
 
 export { AllowedDevelopmentDomains, ServerConfig };
